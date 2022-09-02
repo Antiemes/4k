e8392b8c0b54f0e4b6c8cf1fa71ed850d61fb9e4
Antiemes <vg@electrit.hu>
2022-09-02 17:48:54 +0200

    Delete old exe files

--- x.bat
+++ x.bat
@@ -1,3 +1,6 @@
+del intro-fast.exe
+del intro-slow.exe
+del intro-debug.exe
 git pull
 call build-fast.bat
 call intro-fast.exe
--- y.bat
+++ y.bat
@@ -1,3 +1,6 @@
+del intro-fast.exe
+del intro-slow.exe
+del intro-debug.exe
 git pull
 call build-debug.bat
 call intro-debug.exe
